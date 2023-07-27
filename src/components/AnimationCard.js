c8ca5614eff982ff010cf74b3e02d3e4705eac05
christianHadinata <christianhadinata2004@gmail.com>
2023-07-27 18:59:23 +0700

    Change name Product Name to Product Page

--- src/components/AnimationCard.js
+++ src/components/AnimationCard.js
@@ -4,7 +4,7 @@ const AnimationCard = () => {
   return (
     <div className="md:w-44 md:h-44 w-28 h-28 flex-shrink-0 shadow-md flex flex-col justify-end cursor-pointer hover:scale-110 transform transition-all group">
       <div className="flex md:w-44 w-28 py-[6px] px-[10px] justify-center items-center gap-[10px] bg-[#CBF3FF] group-hover:bg-[#37CFFF] text-[#37CFFF] text-xs font-Poppins font-medium group-hover:text-[#FFF]">
-        <span>Product Name</span>
+        <span>Product Page</span>
       </div>
     </div>
   );
